b84961b508cd645866f802f49af93458753aaf3b
TreNgheDiCode <gabayan170@gmail.com>
2024-05-06 04:25:45 +0700

    Added Dob

--- BlazorIdentityDotNet8/Data/ApplicationUser.cs
+++ BlazorIdentityDotNet8/Data/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace BlazorIdentityDotNet8.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public DateOnly? Dob { get; set; }
     }
 
 }
